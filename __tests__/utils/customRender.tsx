1e960d57be103f6348d3a8acb66db7eb1a279b34
GustaJLemos <gustavolemosmendes@gmail.com>
2023-08-17 22:04:18 -0300

    Criando um render customizado

---
+++ __tests__/utils/customRender.tsx
@@ -0,0 +1,22 @@
+import { ReactElement } from 'react';
+import { CityProvider } from "@contexts/CityContext";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import { RenderOptions, render } from '@testing-library/react-native';
+
+function Providers({ children }: { children: React.ReactNode }) {
+  return (
+    <SafeAreaProvider>
+      <CityProvider>
+        {children}
+      </CityProvider>
+    </SafeAreaProvider>
+  )
+}
+
+const customReader = (
+  ui: ReactElement,
+  options?: Omit<RenderOptions, 'wrapper'>
+) => render(ui, { wrapper: Providers, ...options });
+
+export * from '@testing-library/react-native';
+export { customReader as render, Providers }
